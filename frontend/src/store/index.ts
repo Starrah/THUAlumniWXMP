67a1c6a6ebc1618662550fb5d2b761768eb0db67
mayeths <wow@huanghope.com>
2019-11-21 22:49:46 +0800

    feat: 添加了不完整的高级规则页面
    - 添加`src/pages/newActivity/advanceRule.vue`和`store/module/activity.ts`
    - 高级规则只添加了accept类型，因为没有样式的时候给多个类型会比较头晕。可以先把现在的
      美化了之后再添加audit和reject类型的高级规则，此时不难
    - 在page.json配置advanceRule为一个Tab方便调试，之后确认无误后可以将其入口更改掉

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import profile from "./module/profile";
+import activity from "./module/activity";
 
 Vue.use(Vuex);
 
@@ -10,6 +11,7 @@ export default new Vuex.Store({
     "errMsg": ""
   },
   modules: {
-    profile
+    profile,
+    activity
   }
 })
