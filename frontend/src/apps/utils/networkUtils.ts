af3e4506ce9b40ac100e5751ae983c0bb20d7801
Starrah <46821571+Starrah@users.noreply.github.com>
2019-12-14 00:24:57 +0800

    fix: 修正头像显示url的问题

--- frontend/src/apps/utils/networkUtils.ts
+++ frontend/src/apps/utils/networkUtils.ts
@@ -3,9 +3,8 @@ import apiService from '../../commons/api'
 export function fullUrl(s: string): string{
     if(s.indexOf("://") !== -1)return s;
     else {
-        let firstDot = s.indexOf(".");
         let firstSlash = s.indexOf("/");
-        if (firstDot === -1 || firstDot > firstSlash) {
+        if (firstSlash === 0) {
             s = apiService.baseUrl + s;
         }
         return s;
